08e4b50edb25b7a718e1879a2ff82ca5cbe47903
thidassandaruwan <170103578+thidassandaruwan@users.noreply.github.com>
2026-07-27 19:31:36 +0000

    Fix NoteCard to note detail linking url issue

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -28,7 +28,7 @@ const NoteCard = ({note, setNotes}) => {
     };
 
   return (
-    <Link to={`/note${note._id}`}
+    <Link to={`/note/${note._id}`}
     className="card bg-base-100 hover:shadow-xl transition-all duration-200 border-t-4 border-solid border-primary">
     <div className="card-body border border-primary rounded-xl">
         <h3 className="card-title text-base-content">{note.title}</h3>
