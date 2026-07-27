2e0226e944a7ed382b50eaee78da946e13af6455
thidassandaruwan <170103578+thidassandaruwan@users.noreply.github.com>
2026-07-27 19:07:16 +0000

    Add UI when there are no notes to display

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from "../components/RateLimitedUI";
 import api from "../lib/axios.js"
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -52,6 +53,9 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {/* check if notes are loading, and if so display "Loading notes... text" */}
         {loading && <div className="text-center text-primary  py-10">Loading Notes...</div>}
+
+        {/* if no notes avaiable to shoq */}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
 
         {/* if notes were retrived and we are not ratelimited */}
         {notes.length > 0 && !isRateLimited && (
